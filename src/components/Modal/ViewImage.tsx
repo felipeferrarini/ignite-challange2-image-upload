720a24a0c6feb6948ca84ab64431aca369cd17b9
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2021-10-18 23:18:36 -0300

    chore: completed proposed challenges

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,18 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <Image src={imgUrl} maxW="900px" maxH="600px" />
+
+        <ModalFooter justifyContent="center">
+          <Link target="_blank" href={imgUrl}>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
